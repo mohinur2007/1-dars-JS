b8da2b96b04e0da6ea9271cb4b7ada76311afcaa
Mohinur <hikmatovamohinur2007@gmail.com>
2022-10-08 15:44:08 +0500

    hello

---
+++ mini.js
@@ -0,0 +1,23 @@
+// let number = 15
+// //alert (number);
+// console
+// let age = 19 //number
+// let name = 'najm' //string
+// let boll = true; //boolean
+// console.log(name)
+// let number1; //undefined
+// console.log(number1)
+// console.log (a); //null
+
+
+// let odam ={
+//     nomi: "Islom",
+//     yoshi: 12,
+//     chsksdimi: true
+// }// console.log (odam)
+
+//let X = 5
+//let Y = 6
+//console.log(X+Y)
+
+console.log (1) = undefined
